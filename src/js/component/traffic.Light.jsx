ac948c386a48762b8558fde331bfe9666d4b1501
JuliOli-iu <olivera.julieta1912@gmail.com>
2024-10-12 05:54:35 +0000

    traffic light by ju

---
+++ src/js/component/traffic.Light.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const TrafficLight = () => {
+  const [color, setColor] = useState("red");
+  const [showPurple, setShowPurple] = useState(false);
+
+  const generateColor = () => {
+    const hexColors = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hexColors[Math.floor(Math.random() * hexColors.length)];
+    }
+    return color;
+  };
+
+  console.log(generateColor());
+
+  return (
+    <div className="d-flex flex-column justify-content-start align-items-center"
+      style={{top: 0, position: "fixed", width: "100vw", height: "100vh", backgroundColor: "#5261b3",}}>
+      
+    <div className="bg-dark" style={{ width: "30px", height: "150px" }}></div>
+     
+    <div className="bg-dark rounded-5 d-flex justify-content-evenly flex-column align-items-center"
+      style={{ width: "200px", minHeight: showPurple ? "500px" : "400px" }}>
+
+       {/*  lucesitas  */}  
+        <div
+          onClick={() => setColor("red")}
+          className={"bg-danger rounded-circle " + (color === "red" ? "onLightRed" : "offLight")}
+          style={{ width: "100px", height: "100px" }}
+        ></div>
+
+        
+        <div
+          onClick={() => setColor("yellow")}
+          className={"bg-warning rounded-circle " + (color === "yellow" ? "onLightYellow" : "offLight")}
+          style={{ width: "100px", height: "100px" }}
+        ></div>
+
+        <div
+          onClick={() => setColor("green")}
+          className={"bg-success rounded-circle " + (color === "green" ? "onLightGreen" : "offLight")}
+          style={{ width: "100px", height: "100px" }}
+        ></div>
+
+      
+        {showPurple && (
+          <div
+            onClick={() => setColor("purple")}
+            className={"bg-purple rounded-circle " + (color === "purple" ? "onLightPurple" : "offLight")}
+            style={{ backgroundColor: "purple", width: "100px", height: "100px" }}
+          ></div>
+        )}
+      </div>
+
+       {/* botoncito */}
+      <button
+        className="btn btn-light mt-3"
+        onClick={() => setShowPurple(true)} 
+      >
+        Agregar luz violeta!
+      </button>
+    </div>
+  );
+};
+
+export default TrafficLight;
